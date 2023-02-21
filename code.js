f83b66112aada31980b4a2a04e049a99887e3984
ski235 <110300995+ski235@users.noreply.github.com>
2023-02-21 18:15:16 +0100

    c

---
+++ code.js
@@ -0,0 +1,15 @@
+function login (){
+let username= prompt("username");
+let password = prompt("password");
+if (username=='Yuna'&& password=='Yuna'){
+    window.location.href="yrenzetti.html"
+}
+
+if (username=='urenzetti'&&password=='2352323'){
+    window.location.href='urenzetti.html'
+
+}
+if (username=='papa'&&password=="yuna"){
+    window.location.href="wrenzetti.html"
+}
+}
